0e2fe12524158fade4f4cad0fe37eaec6f73d98b
mamarante-4444 <mamarante@itba.edu.ar>
2026-05-16 16:38:15 -0300

    Delete 06_InfoMed_TP4_Amarante_Quiroga.sql

--- 06_InfoMed_TP4_Amarante_Quiroga.sql
+++
@@ -1,9 +0,0 @@
--- Consulta 6
-
-SELECT
-    nombre,
-    calle || ' ' || numero AS direccion
-FROM Pacientes
-WHERE TRIM(ciudad) ILIKE 'Buenos %Aires%'
-   OR TRIM(ciudad) ILIKE 'Buenos Aiers%'
-   OR TRIM(ciudad) ILIKE 'Bs%Aires%';
